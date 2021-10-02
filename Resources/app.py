78433c55f8bb6131471e0df39204dbe55aeb362a
adrianlongoria <strongmachine@icloud.com>
2021-10-01 21:41:32 -0500

    completed APIs

--- Resources/app.py
+++ Resources/app.py
@@ -1,3 +1,6 @@
+##################################################################################################################
+##################################################################################################################
+##################################################################################################################
 import numpy as np
 
 import sqlalchemy
@@ -70,15 +73,15 @@ def welcome():
         f"When given the start and the end date (YYYY-MM-DD), calculate the min, avg, and max temperature for dates between the start and end dates provided</a></li></ol><br/>"
       
     )
-
-
+##################################################################################################################
+##################################################################################################################
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     "Query to retrieve the last 12 months of precipitation data and return the results."
-    # Create our session (link) from Python to the DB.
+     # Create session link
     session = Session(engine)
 
-    # Calculate the date 1 year ago from the last data point in the database.
+    # Calculate the date 1 year from the last data point in the database.
     last_measurement_data_point_tuple = session.query(
     Measurement.date).order_by(Measurement.date.desc()).first()
     (latest_date, ) = last_measurement_data_point_tuple
@@ -86,7 +89,7 @@ def precipitation():
     latest_date = latest_date.date()
     date_year_ago = latest_date - relativedelta(years=1)
 
-    # Perform a query to retrieve the data and precipitation scores.
+    # Perform a query to retrieve the data and prcp scores.
     data_from_last_year = session.query(Measurement.date, Measurement.prcp).filter(
         Measurement.date >= date_year_ago).all()
 
@@ -100,17 +103,17 @@ def precipitation():
             precip_dict[date] = prcp
             all_prcp.append(precip_dict)
 
-    # Return the JSON representation of dictionary.
+    # Return the JSON dictionary.
     return jsonify(all_prcp)
-
-
+##################################################################################################################
+##################################################################################################################
 @app.route("/api/v1.0/tobs")
 def tobs():
     "Query for the dates and temperature observations from a year from the last data point for the most active station."
-    # Create our session (link) from Python to the DB.
+    # Create session link
     session = Session(engine)
 
-    # Calculate the date 1 year ago from the last data point in the database.
+    # Calculate the date 1 year from the last data point in the database.
     last_measurement_data_point= session.query(
         Measurement.date).order_by(Measurement.date.desc()).first()
     (latest_date, ) = last_measurement_data_point
@@ -129,27 +132,27 @@ def tobs():
     print(
         f"Station id of the most active station is {most_active_station_id}.")
 
-    # Perform a query to retrieve the data and temperature scores for the most active station from the last year.
+    # Perform a query to retrieve the data and temperature scores for the most active station from the year before.
     data_from_last_year = session.query(Measurement.date, Measurement.tobs).filter(
         Measurement.station == most_active_station_id).filter(Measurement.date >= date_year_ago).all()
 
     session.close()
 
-    # Convert the query results to a dictionary using date as the key and temperature as the value.
+    # Convert the query dictionary using date as the key and temp as the value.
     all_temperatures = []
     for date, temp in data_from_last_year:
         if temp != None:
             temp_dict = {}
             temp_dict[date] = temp
             all_temperatures.append(temp_dict)
-    # Return the JSON representation of dictionary.
+    # Return the JSON dictionary.
     return jsonify(all_temperatures)
-
-
+##################################################################################################################
+##################################################################################################################
 @app.route("/api/v1.0/stations")
 def stations():
     "Return a JSON list of stations from the dataset."
-    # Create our session (link) 
+    # Create session link
     session = Session(engine)
 
     # Query stations.
@@ -158,7 +161,7 @@ def stations():
 
     session.close()
 
-    # Convert the query results to dictionary.
+    # Convert to dictionary.
     all_stations = []
     for station, name, latitude, longitude, elevation in stations:
         station_dict = {}
@@ -171,15 +174,15 @@ def stations():
 
     # Return the JSON dictionary.
     return jsonify(all_stations)
-
-
+##################################################################################################################
+##################################################################################################################
 @app.route('/api/v1.0/<start>', defaults={'end': None})
 @app.route("/api/v1.0/<start>/<end>")
 def determine_temps_for_date_range(start, end):
     "Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range."
     "When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date."
     "When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive."
-    # Create our session 
+      # Create session link
     session = Session(engine)
 
     # If we have both a start date and an end date.
@@ -212,9 +215,9 @@ def determine_temps_for_date_range(start, end):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
+##################################################################################################################
+##################################################################################################################
+##################################################################################################################
+##################################################################################################################
 
 
